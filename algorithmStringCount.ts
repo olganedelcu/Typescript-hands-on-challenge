0c7ca6cf97fd778de4bda900a9af4a9c156d3e47
Olga <olganedelcuam@gmail.com>
2022-10-18 17:37:04 +0200

    added functionality

---
+++ algorithmStringCount.ts
@@ -0,0 +1,21 @@
+/*
+ * the number of times each character appears in the string below
+ * {
+ *   "a": 7,
+ *   "b": 5,
+ *   ...
+ * }
+ */
+
+const characters: string = "abcaabfbcubcbamabcabc";
+
+// split the text into an array of characters
+// use of the reduce method over the array to compute the counts: reduce(reducer, initialValue)
+
+function countChars(text: string) {
+  return text.split("").reduce((map, char) => {
+    map[char] = (map[char] || 0) + 1;
+    return map;
+  }, {}); // initial value empty object
+}
+console.log(countChars(characters));
