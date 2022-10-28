5d3ec5b07a33db4a4f6ea32382676b40fbad0f3d
Olga <olganedelcuam@gmail.com>
2022-10-28 15:50:03 +0200

    Garbage collected and removeEventListeners concept

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
-document.getElementById("clickMe").addEventListener("click", function xyz() {
+/* document.getElementById("clickMe").addEventListener("click", function xyz() {
   console.log("Button Clicked");
-  document.getElementById("heading").innerHTML = "Hey Clicked";
 });
+// xyz will push in the call stack and will be executed.
+ */
+/* // count how many time the button is cliked and increase count.
+let count = 0;
+document.getElementById("clickMe").addEventListener("click", function xyz() {
+  console.log("Button Clicked", ++count);
+}); // -> not a good solution
+ */
+
+
+// Closure
+
+function attachEventListeners() {
+  let count1 = 0;
+  document.getElementById("clickMe").addEventListener("click", function xyz() {
+    console.log("Button Clicked", ++count1);
+  });
+};
+
+attachEventListeners();
